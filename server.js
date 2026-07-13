d02515edc17a53e7c530884e068756e8b6868b6a
pandikani <pandikani636@gmail.com>
2026-07-13 10:39:43 +0530

    Add vercel config

--- server.js
+++ server.js
@@ -1,48 +1,46 @@
-const path = require('path');
-const express = require('express');
-const cloudinary = require('cloudinary');
+const path = require("path");
+const express = require("express");
+const cloudinary = require("cloudinary");
 const app = require("./app");
-const connectDatabase = require('./config/database');
-const PORT = process.env.PORT || 4000;
+const connectDatabase = require("./config/database");
 
-// UncaughtException Error
-process.on('uncaughtException', (err) => {
+// Uncaught Exception
+process.on("uncaughtException", (err) => {
     console.log(`Error: ${err.message}`);
     process.exit(1);
 });
 
+// Connect Database
 connectDatabase();
 
+// Cloudinary Config
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-// deployment
+// Deployment
 __dirname = path.resolve();
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/frontend/build')))
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "frontend", "build")));
+
+    app.get("/", (req, res) => {
+        res.json({
+            success: true,
+            message: "Buyer Seller Backend API Running 🚀",
+        });
     });
 } else {
-    app.get('/', (req, res) => {
-        res.send('Server is Running! 🚀');
+    app.get("/", (req, res) => {
+        res.send("Server is Running! 🚀");
     });
 }
 
-const server = app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`)
-});
-
-// Unhandled Promise Rejection
-process.on('unhandledRejection', (err) => {
+// Handle Unhandled Promise Rejections
+process.on("unhandledRejection", (err) => {
     console.log(`Error: ${err.message}`);
-    server.close(() => {
-        process.exit(1);
-    });
 });
 
-
+module.exports = app;
